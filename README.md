023f6af8b882cdf97efa2a021817107b48f344a4
Eoin McGrath <eoin.mcg@gmail.com>
2016-09-11 21:14:57 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Day of the glitch
+
+Entry for [#js13kgames](https://js13kgames.com)
+
+By [@eoinmcg](https://twitter.com/eoinmcg)
+
+![Screenshot](https://github.com/eoinmcg/dayoftheglitch/raw/master/screeenshot.gif)
---
+++ js/engine/particle.js
@@ -0,0 +1,48 @@
+class Particle extends Sprite {
+
+  constructor(g, o) {
+    super(g, o);
+
+    this.name = 'particle';
+    this.scale = 1;
+    this.group = 'na';
+
+    this.w = 4;
+    this.h = 4;
+
+    this.lifespan = $.H.rnd(20,50);
+    this.ttl = this.lifespan;
+    this.alpha = 1;
+
+
+    this.vx = o.vx || ( $.H.rnd(0, 300) - 300 );  
+    this.vy = o.vy || ( $.H.rnd(0, 300) * -1 );  
+  }
+
+
+  update(step) {
+    super.update(step);
+
+    this.x += this.vx * step; 
+    this.y += this.vy * step; 
+
+
+    this.ttl -= 1;
+    if (this.ttl < 0) {
+      this.remove = true;  
+    }
+
+  }
+
+
+  render() {
+
+    var g = this.g;
+
+
+    g.draw.rect(this.x, this.y, 5, 5, $.cols[this.col]);
+
+  }
+
+
+}
